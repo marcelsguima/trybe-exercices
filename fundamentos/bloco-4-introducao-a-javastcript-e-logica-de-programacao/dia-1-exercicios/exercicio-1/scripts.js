057649b38c1173aa8f53f6a4708c237e7083c8c4
marceslguima <marcelsguima@gmail.com>
2022-06-21 18:21:35 -0300

    criado diretório exercicios 4.1

---
+++ fundamentos/bloco-4-introducao-a-javastcript-e-logica-de-programacao/dia-1-exercicios/exercicio-1/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Marcel";
+const birthCity = "Pedro Leopoldo";
+let birthYear = 1991;
+birthYear = 2030;
+birthCity = "Vespasiano";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/bloco-4-introducao-a-javastcript-e-logica-de-programacao/dia-1-exercicios/exercicio-4/scripts.js
@@ -0,0 +1,18 @@
+let estado = "aprovada";
+switch (estado) {
+
+    case "aprovada":
+        console.log("aprovada");
+        break;
+
+    case "lista":
+        console.log("lista")
+        break; 
+
+    case "reprovada":
+        console.log("reprovada")
+        break;         
+        
+    default:
+        console.log("Não se aplica")
+}
