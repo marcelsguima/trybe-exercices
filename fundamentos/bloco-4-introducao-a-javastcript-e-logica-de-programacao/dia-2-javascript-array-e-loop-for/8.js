b3b6ed9f44f7895b2788b3d5942d420708e02122
marceslguima <marcelsguima@gmail.com>
2022-06-22 22:32:06 -0300

    exercicio 4.2 completos

---
+++ fundamentos/bloco-4-introducao-a-javastcript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/8.js
@@ -0,0 +1,7 @@
+let contagem = [];
+
+for (let index = 1; index <= 25; index += 1){
+    contagem.push(index);
+}
+
+console.log(contagem);
---
+++ fundamentos/bloco-4-introducao-a-javastcript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/9.js
@@ -0,0 +1,11 @@
+let contagem = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  contagem.push(index);
+}
+
+console.log(contagem);
+
+for (let index = 0; index < contagem.length; index += 1) {
+    console.log(contagem[index] / 2);
+  };
